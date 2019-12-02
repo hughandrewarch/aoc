ca4199c217f301dcc0174e10cf913c615d2a6516
hughandrewarch <hughandrewarch@gmail.com>
2019-12-02 16:55:18 -0500

    Day2 complete
    nothing special, akward functionality to test
    should revisit and break into reading 4 pieces at a time

---
+++ 2019/src/main/java/day02/Main.java
@@ -0,0 +1,27 @@
+package day01;
+
+import util.MyReader;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String args[]) {
+        String file = "src/main/resources/data/day01/input.txt";
+        MyReader fileReader = new MyReader(file);
+        List<Integer> data = fileReader.readLinesInt();
+
+        int calculatedSum = data.stream()
+                .map(FuelCounter::calculate)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Part A Sum: " + calculatedSum);
+
+        int calculatedRecursiveSum = data.stream()
+                .map(FuelCounter::calculateRecursive)
+                .reduce(0, Integer::sum);
+
+        System.out.println("Part B Sum: " + calculatedRecursiveSum);
+
+    }
+}
